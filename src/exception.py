5df70956ac1914a9278bf651ee756e1220925a03
asrohan5 <rohanrohu99@gmail.com>
2026-04-15 23:00:04 +0530

    Update 1

--- src/exception.py
+++ src/exception.py
@@ -1,31 +1,31 @@
-import sys
-from src.logger import logging
+"""
+Custom exception class
+"""
 
-def error_message_details(error, error_detail:sys):
-    exc_type, exc_obj, exc_tb = error_detail.exc_info()
+import sys
+import logging
+
+
+def _get_error_details(error: Exception, error_detail: sys) -> str:
+    """Extract file name, line number, and message from an exception."""
+    _, _, exc_tb = error_detail.exc_info()
     if exc_tb is not None:
         file_name = exc_tb.tb_frame.f_code.co_filename
         line_number = exc_tb.tb_lineno
     else:
-        file_name = 'Unknown'
-        line_number = 'Unknown'
-    
-    error_message = f"Error Occured in Python Script Name [{file_name}] line number [{line_number}] error message [{str(error)}]"
-    
-    return error_message
+        file_name = "Unknown"
+        line_number = "Unknown"
+    return (
+        f"Error in [{file_name}] at line [{line_number}]: {str(error)}"
+    )
+
 
 class CustomException(Exception):
-    def __init__(self, error_message,error_detail:sys):
+
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(str(error_message))
-        self.error_message = error_message_details(error_message, error_detail=error_detail)
+        self.error_message = _get_error_details(error_message, error_detail)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.error_message
-    
 
-#if __name__ == "__main__":
-#    try:
-#        a = 1/0
-#    except Exception as e:
-#        logging.info("divide by zero error")
-#        raise CustomException(e,sys)
